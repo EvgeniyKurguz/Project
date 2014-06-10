a6dd81812e3e576d9339b742a9f9832caae15d64
EvgeniyKurguz <EvgeniyKurguz>
2014-06-10 12:24:11 +0600

    Runner

--- src/Runner.java
+++ src/Runner.java
@@ -2,6 +2,6 @@
 public class Runner {
     public static void  main(String[] arg){
         System.out.println("J34hsdfbh");
-        System.out.println("J34hsdfbh");
+        System.out.println("How are you");
     }
 }
